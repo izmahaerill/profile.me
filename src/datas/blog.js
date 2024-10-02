e1cac314d6e3acde7d54938d5b2ba05841859ed3
azmi <azmik5671@gmail.com>
2024-10-02 23:08:18 +0800

    update blog page

---
+++ src/datas/blog.js
@@ -0,0 +1,31 @@
+const blog = [
+  {
+    id: 1,
+    time: "April 1, 2023",
+    views: "2703 views",
+    title: "NextAuth.js behinds a proxy",
+    subTitle: "NextAuth.js",
+    information:
+      "A step-by-step guide for using Auth.js in a subpath behind a Netlify proxy",
+  },
+  {
+    id: 2,
+    time: "October 20, 2022",
+    views: "2703 views",
+    title: "NextAuth.js behinds a proxy",
+    subTitle: "NextAuth.js",
+    information:
+      "A step-by-step guide for using Auth.js in a subpath behind a Netlify proxy",
+  },
+  {
+    id: 3,
+    time: "October 10, 2021",
+    views: "2703 views",
+    title: "NextAuth.js behinds a proxy",
+    subTitle: "NextAuth.js",
+    information:
+      "A step-by-step guide for using Auth.js in a subpath behind a Netlify proxy",
+  },
+];
+
+export default blog;
--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "../layouts/layout";
 import Input from "@/components/micro/Input";
+import datablog from "@/datas/blog.js";
 
 export default function Blog() {
   return (
@@ -10,6 +11,19 @@ export default function Blog() {
           All Posts
         </h1>
         <Input />
+        {datablog.map((blogs) => (
+          <div key={blogs.id} className="flex items-start py-8 text-secondary">
+            <div className="left w-1/4">
+              <h4>{blogs.time}</h4>
+              <h5 className="text-sm">{blogs.views}</h5>
+            </div>
+            <div className="right">
+              <h1 className="text-3xl font-bold text-white">{blogs.title}</h1>
+              <h2 className="text-lg text-tersier">{blogs.subTitle}</h2>
+              <h3 className="mt-3">{blogs.information}</h3>
+            </div>
+          </div>
+        ))}
       </Layout>
     </>
   );
